f2c33528c3f01e58f7d87a4cd2332e3cd66e8900
krishnan-srinivasan <krishnan1994@gmail.com>
2014-09-16 20:29:41 -0400

    homework 1 started

--- week1/script.js
+++ week1/script.js
@@ -1,1 +1,22 @@
-alert("helloworld");
+var helloUniverse = function() {
+	console.log("hello universe");
+};
+
+var callTwice = function(a) {
+	a();
+	a();
+}
+
+//code does not always run in code-line order, asynchronous funcs start a task
+//and run it in background, when they finish they call a callback function, 
+//anything that is called after an async function
+
+$.get("http://reddit.com/hot.json"), function(response){
+	var stories = response.data.children;
+	for (var i in stories) {
+		var story = stories[i].data;
+
+	}
+}
+
+response();
