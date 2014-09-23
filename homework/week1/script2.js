8530aacc057b3d2b9c77943c57e1feadb9456b3e
ellensu <ellenfsu@gmail.com>
2014-09-23 17:33:10 -0400

    Uploaded homework!

--- homework/week1/script2.js
+++ homework/week1/script2.js
@@ -1,4 +1,5 @@
 console.log("javascript working!");
+
  // Just to test. 
 $(document).ready(function() {
     $("#refresh-button").click(function() {
@@ -6,24 +7,24 @@ $(document).ready(function() {
       console.log("refreshed!");
       window.location.reload();
       });
-  $.get("http://www.reddit.com/hot.json", function(response) {
+  $.get("https://api.instagram.com/v1/tags/search?q=snowy&access_token=1507558611.d8a3de1.1390d45eb8754206a04dc586b824700b", function(response) {
     // If you don't understand line 6, you may want to read up
     // on Javascript objects, in the slides or internet. 
-    var stories = response.data.children;
-    for(var i in stories) {
-      story = stories[i].data;
-      var newStory = $("<li><a></a></li>")
-        .find("a")
-        .attr("href", "http://www.reddit.com/" + story.permalink)
-        .html(story.title)
+    var catPicture = response.data.children;
+    for(var i in pictures) {
+      picture = pictures[i].data;
+      var cat = $("<li><img></li>")
+        .find("img")
+        .attr("src", picture.images.standard_resolution.url)
+        .attr("alt", "cats")
         .end();
-      var upVotes = $("<li>" + story.ups + " upvotes" + "</li>");
+      //var upVotes = $("<li>" + story.ups + " upvotes" + "</li>");
       // Your code here:
       // In the data in the Javascript object 'story',
       // find the title, permalink, and upvote count
       // Then create HTML elements with jQuery (like in line 9)
       // and append them to the #list element. 
-      $("#list").append(newStory,upVotes,"<br>");
+      $("#list").append(cat,"<br>");
       // Look at the JS console in Chrome to see what story looks like
       console.log(story);
 
