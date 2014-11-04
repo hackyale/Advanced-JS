cce297e884374d78b390f59f1287fb5e55edffb6
jinchentian <chentian.jin@yale.edu>
2014-11-04 18:46:31 -0500

    Create script.js

---
+++ homework/week1/script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+  <!-- Make sure to include jQuery *before* your own Javascript -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="./script.js"></script>
+</head>
+<body>
+  <h1>Top Reddit Stories:</h1>
+  <button id="refresh-button">Reddit</button>
+  <button id="news-button">NYTimes</button>
+  <ul id="list"></ul>
+</body>
+</html>
